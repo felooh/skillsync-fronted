176f91631696eadd8061348e9f3f49d71ff9803b
Felix Gathage <gathagefelix@gmail.com>
2024-11-14 15:03:47 +0300

    done

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,9 +3,13 @@
 # navigate to app folder
 cd /app
 
+# Remove any existing node and npm installations
+apt-get remove --purge -y nodejs npm
+apt-get autoremove -y
+
 # install node and npm
 apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install nodejs -y
 apt-get install npm -y
 apt install nginx -y
